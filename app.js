714437fdf9741c88a595679b7e0448ff47412e55
Arthur Jamain <a.jamain.pro@gmail.com>
2020-09-12 16:11:14 +0200

    En attente de typescript

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var mustacheExpress = require('mustache-express');
 
-var app = express();app.get('/', function (req, res) {
-    res.send('Hello World!');
+var app = express();
+app.set('port', 3000);
+app.engine('html', mustacheExpress());
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// ROUTINGE
+
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+// EUKAI LE START
+
+app.listen(app.get('port'), function () {
+    console.log('GGICS listening on port ' + app.get('port') + '!');
 });
